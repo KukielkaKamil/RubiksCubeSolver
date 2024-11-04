20ea8f7fd7748dc2668cac1ac69ff8c70978f7ba
KukielkaKamil <kukielka.kamil77@gmail.com>
2024-11-04 21:55:05 +0100

    some machine learning stuff and added new LBL aproach

--- kociemba.py
+++
@@ -1,6 +0,0 @@
-import twophase.solver as sv
-
-# cubestring = 'DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL'
-def solve(cubestring):
-    solution = sv.solve(cubestring,19,0)
-    return solution
